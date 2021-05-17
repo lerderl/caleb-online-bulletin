afa924b88eb666010e5a9161cc43028b1922e01c
lerderl <josepholukunle1107@gmail.com>
2021-05-17 14:17:34 +0100

    Added validation with styling and some layout cleanup

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,3 +1,4 @@
 class Notice < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 6, maximum: 100}
   has_many_attached :files         # Use has_many_attached for multiple files allowed
 end
